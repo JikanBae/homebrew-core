d569f8ff25aa6a14d9135c2abbf602aba49933bd
MARUI Atsushi <marui@ms.geidai.ac.jp>
2016-06-23 20:06:35 +0900

    updated to version 0.310

--- Formula/takt.rb
+++ Formula/takt.rb
@@ -1,15 +1,8 @@
 class Takt < Formula
   desc "text-based music programming language"
   homepage "http://takt.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/takt/takt-0.309-src.tar.gz"
-  sha256 "44fca667d610fba15deb441dd44e5b87dabbe5619d7fa8a13f5c8c1f054dd509"
-
-  bottle do
-    sha256 "0669543315f8ab5da5eadacdd131c76c4b20a11502fd086db0b052664eb334b2" => :el_capitan
-    sha256 "3dbf255bf638d233a4c4eda5823bc3f4fa4d5fd6985ea25022edafef6864ce1d" => :yosemite
-    sha256 "3b95c5b824c3616c6278d02718f511013aa6f1f921d04de62c5e674fae27aa1a" => :mavericks
-    sha256 "12faf048c8ce38aa97ca1cd3e95aad70c8430315ecfd76a1c11208a0874ce87c" => :mountain_lion
-  end
+  url "https://downloads.sourceforge.net/project/takt/takt-0.310-src.tar.gz"
+  sha256 "eb2947eb49ef84b6b3644f9cf6f1ea204283016c4abcd1f7c57b24b896cc638f"
 
   depends_on "readline"
 
